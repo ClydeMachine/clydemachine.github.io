0206e66c075ea0b7f282d13b9285dd396fb6b795
Clyde Machine <clydemachine@gmail.com>
2026-02-16 19:33:46 -0800

    Add missing JS assets, clean up index.

---
+++ js/cntct.js
@@ -0,0 +1,12 @@
+document.write('<a href="mai'); //Eat it, bots.
+document.write('lto:');
+document.write('clydemachine');
+document.write('@');
+document.write('gmail');
+document.write('.');
+document.write('com">');
+document.write('clydemachine');
+document.write('@');
+document.write('gmail');
+document.write('.');
+document.write('com</a>'); //Choke on it.
